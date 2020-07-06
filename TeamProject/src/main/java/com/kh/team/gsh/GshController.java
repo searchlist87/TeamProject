1366591438a09bf0507c68b42aea97f7d08725c3
KH <KH@KH-PC>
2020-07-06 14:54:42 +0900

    commit

---
+++ TeamProject/src/main/java/com/kh/team/gsh/GshController.java
@@ -0,0 +1,16 @@
+package com.kh.team.gsh;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/gsh")
+public class GshController {
+	
+	@RequestMapping(value = "/register" , method = RequestMethod.GET)
+	public void register() throws Exception {
+		
+	}
+
+}
