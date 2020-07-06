33ba2d79cd8d2946b7ec0f35115b24fa8fe10a2f
KH <KH@KH-PC>
2020-07-06 15:55:07 +0900

    tag + blog-single 1차 수정

--- TeamProject/src/main/java/com/kh/team/HomeController.java
+++ TeamProject/src/main/java/com/kh/team/HomeController.java
@@ -12,7 +12,7 @@ public class HomeController {
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		return "/templet/shop-grid";
+		return "/templet/blog-single-sidebar";
 	}
 	
 }
