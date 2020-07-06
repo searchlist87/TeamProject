5611097e9d32050e258cfda17799be82c3f771e3
KH <KH@KH-PC>
2020-07-06 14:55:02 +0900

    테스트2

--- TeamProject/src/main/java/com/kh/team/sgh/SgnController.java
+++ TeamProject/src/main/java/com/kh/team/sgh/SgnController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/sgh")
+@RequestMapping("/sgh/board") // board 댓글
 public class SgnController {
 
 	@RequestMapping(value="/register", method=RequestMethod.GET)
