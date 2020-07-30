7ec3f63b9868b6b5fac6693eda6d34462b3851e8
KH <KH@KH-PC>
2020-07-30 13:54:40 +0900

    고수현 커밋

--- TeamProject/src/main/java/com/kh/team/controller/GshController.java
+++ TeamProject/src/main/java/com/kh/team/controller/GshController.java
@@ -68,13 +68,13 @@ public class GshController {
 		return "success";
 	}
 	
-	// 리뷰 삭제 원본
+	// 리뷰 삭제
 	// 삭제를 위해서 리뷰 번호를 ()에 넣어준다
 	@RequestMapping(value = "/delete_review", method = RequestMethod.GET)
 	public String reviewDelete(int review_num, HttpSession session) throws Exception {
 		String user_id = (String)session.getAttribute("user_id");
-		System.out.println("user_id :" + user_id);
-		System.out.println("review_num:" + review_num);
+//		System.out.println("user_id :" + user_id);
+//		System.out.println("review_num:" + review_num);
 		gshReviewService.delete_review(review_num, user_id);
 		return "user/gsh/movie/movieInfo";
 	}
@@ -83,7 +83,6 @@ public class GshController {
 	@RequestMapping(value = "/movieView", method = RequestMethod.GET)
 	public String movieView(Model model) throws Exception {
 		List<GshMovieDto> list = gshMovieService.select_movieAll();
-		
 //		System.out.println(list);
 		model.addAttribute("list", list);
 		return "user/gsh/movie/movieView";
@@ -93,7 +92,7 @@ public class GshController {
 	@RequestMapping(value = "/movieInfo", method = RequestMethod.GET)
 	public String movieInfo(HttpSession session, String movie_code, ModelMap model) throws Exception {
 		GshMovieDto movieDto = gshMovieService.selectMovieCode(movie_code);
-		System.out.println("movieInfo, movieDto:" + movieDto);
+//		System.out.println("movieInfo, movieDto:" + movieDto);
 		// 스틸컷 파일명 목록
 		List<String> subImageList = gshMovieService.selectMovieSubImage(movie_code);
 		List<GshReviewVo> reviewList = gshReviewService.selectReviewByCode(movie_code);
@@ -105,7 +104,16 @@ public class GshController {
 		return "user/gsh/movie/movieInfo";
 	}
 	
-	// 리뷰 목록
+	// 영화 이름으로 영화 찾기
+	public void findMovieName() throws Exception {
+	}
+	
+	// 영화 장르로 영화 찾기
+	public void findMovieGenre() throws Exception {
+	}
+	
+	
+	// 영화 코드로 리뷰 찾기
 	@RequestMapping(value = "/reviewList/{movie_code}", method = RequestMethod.GET)
 	@ResponseBody
 	public List<GshReviewVo> reviewList(@PathVariable("movie_code") String movie_code) throws Exception {
@@ -120,15 +128,5 @@ public class GshController {
 		String user_id = (String) session.getAttribute("user_id");
 		gshReviewService.update_review(review_content, review_score, review_num, user_id);
 	}
-	
-	
-	
-	// 상영작 리스트
-//	@RequestMapping(value = "/onairList", method = RequestMethod.GET)
-//	public String onairList(Model model) throws Exception {
-//		List<GshMovieDto> list = gshMovieService.select_movieAll();
-//		model.addAttribute("list", list);
-//		return "user/gsh/movie/onairList";
-//	}
 	
 }
